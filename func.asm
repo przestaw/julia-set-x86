cc0b08ced75fd914154dcfea96f601d05e74b815
przestaw <pstawczy@mion.elka.pw.edu.pl>
2019-01-05 23:46:45 +0100

    TODO: align data on 16, repair algorithm

--- func.asm
+++ func.asm
@@ -2,12 +2,17 @@ global  func
 
 section .data
 
-color_B: equ     3
-color_G: equ     7
-color_R: equ     5
+color_B: equ     1
+color_G: equ     4
+color_R: equ     2
 
-const_R     db 0x20 ;
-
+align 32
+CONST   DQ 0.66 , 0.012
+DOUBLE_RADIUS    DQ 3.0
+STEP    DQ 0.002
+BEGIN   DQ -0.90
+BEGIN_M   DQ -0.90
+NEGATE      DQ 0x8000000000000000
 section	.text
 
 func:
@@ -25,22 +30,59 @@ func:
     je      error
     cmp     RSI, 0 ;check if array is null
     je      error
+;ASSUMING STRUCT IS VALID
+    xorpd   xmm14, xmm14
+    xorpd   xmm15, xmm15
+    movlpd  xmm14, [STEP];[RDI+24] ;load Immaginary step
+    movhpd  xmm15, [STEP];[RDI+16] ;load Real step
+    movapd  xmm4, [CONST]
+    xorpd   xmm13, xmm13
+    movlpd  xmm13, [BEGIN];[RDI+40] ;load starting position
+    movhpd  xmm13, [BEGIN_M];[RDI+32]
 ;THIS IS BEGIN OF
-    mov     RCX, QWORD [RDI+8]
+    mov     RCX, QWORD [RDI+8] ;load resolution on Y
 loop_Y:
-    mov     RBX, QWORD [RDI]
+    mov     RBX, QWORD [RDI] ;load resolution on X
+    movhpd  xmm13, [BEGIN_M];[RDI+32] ;load begin of the line
     cmp     RCX, 0
     je      fin
+    addpd   xmm13, xmm14 ;move to the next line
     dec     RCX
 loop_X:
     cmp     RBX, 0
     je      loop_Y
+    addpd   xmm13, xmm15 ;move to next pixel in line
     dec     RBX
 prepare:
-;TODO - load floats
+    mov     R8W, 0
+    movapd  xmm0, xmm13 ;load the pixel
+    movapd  xmm1, xmm0
+    mulpd   xmm1, xmm1 ;   Re^2  |  Im^2
 compute:
-;TODO - algorithm
+    ;calc new Z
+    movhlps xmm2, xmm0 ;  ?????  |   Re
+    mulsd   xmm2, xmm0 ;  ?????  |  Re*Im
+    movapd  xmm3, xmm1 ;   Re^2  |  Im^2
+    movsd   xmm3, xmm2 ;   Re^2  |  Re*Im
+    movlhps xmm2, xmm1 ;   Im^2  |  Re*Im
+    xorps   xmm0, xmm0 ;
+    movhpd  xmm0, [NEGATE] ;Is there a easier way to negate?
+    xorpd   xmm2, xmm0 ;  -Im^2  |  Re*Im
+    addpd   xmm2, xmm3 ;Re^2+Im^2| 2*Re*Im - aka New:Re|New:Im
+    ;TODO: add CONST from register xmm4
+    addpd   xmm2, xmm4
+    movapd  xmm0, xmm2 ; save new computed Z !
+    movapd  xmm1, xmm0 ;   Re    |   Im
+    mulpd   xmm1, xmm1 ;   Re^2  |  Im^2
+
+    movhlps xmm2, xmm1 ; ??????? |  Re^2
+    addsd   xmm2, xmm1 ; ??????? |Re^2+Im^2
     add     R8W, 1
+    cmp     R8, 550
+    jge     save
+    comisd  xmm2, [DOUBLE_RADIUS];[RDI+48]
+    jb      compute
+
 ;TODO - je prepare
 save:
     mov     RAX, 0
--- main.cpp
+++ main.cpp
@@ -2,53 +2,78 @@
 #include <fstream>
 //#include <cstdio>
 
+struct __attribute__((__packed__)) DIB{
+    __uint16_t TYPE = 19778;
+    __uint32_t FILE_SIZE = 0;/////
+    __uint16_t R1 = 0;
+    __uint16_t R2 = 0;
+    __uint32_t OFFSET = 54;
+    __uint32_t DIBSIZE = 40;
+    __uint32_t width = 0;///
+    __uint32_t height = 0;///
+    __uint16_t planes = 1;
+    __uint16_t BPP = 24;
+    __uint32_t BI_RGB = 0;
+    __uint32_t RAW_SIZE = 0;///
+    __uint32_t RES_X = 0;
+    __uint32_t RES_Y = 0;
+    __uint32_t NO_COLOR = 0;
+    __uint32_t IMPOR_COLOR = 0;
+};
+
 struct data_julia
 {
     __int64_t width;
     __int64_t height;
-    double left_down_R;
-    double left_down_I;
-    double right_up_R;
-    double right_up_I;
+    double step_R;
+    double step_I;
+    double left_up_R;
+    double left_up_I;
+    double radius;
 };
-
-char output_str[256];
-char input[256];
 
 extern "C"
 {
   int func(data_julia *argument, char *array);
 }
 
+DIB calc_DIB_and_padding(data_julia &data)
+{
+  DIB ret;
+  ret.height = data.height;
+  ret.width = data.width;
+  u_int pad = (ret.width*3)%4;
+  ret.RAW_SIZE = (ret.width + pad) * ret.height; //?? unnescesary
+  ret.FILE_SIZE = ret.RAW_SIZE + ret.OFFSET;
+  return ret;
+}
+
 int main() {
     data_julia arg;
-    arg.width = 300;
-    arg.height = 300;
-    arg.left_down_R = -1.5;
-    arg.left_down_I = -1.5;
-    arg.right_up_R = 1.5;
-    arg.right_up_I = 1.5;
-    char image[270000] = {}; //300x300
-    char dib[54] = {};
+    arg.width = 900;
+    arg.height = 900;
+    arg.step_R = 2;
+    arg.step_I = 2;
+    arg.left_up_R = -10.0;
+    arg.left_up_I = 10.0;
+    arg.radius = 40000000;
+    //char image[2430000]; //300x300
+    DIB dib = calc_DIB_and_padding(arg);
+    //char dib[54] = {};
+            std::cout << dib.RAW_SIZE;
+    char * image= new char[dib.RAW_SIZE*3];
     std::fstream file;
-    file.open("../hed.mine", std::ios::in);//BMP 300x300 RGB uncompressed header
-    file.read(dib, 54);
-    std::cout << file.gcount();
-    file.close();
-
     std::cout << "Size of argument : " << sizeof(arg) << '\n';
     //TODO: CALL
     std::cout << "result : " << func(&arg, &image[0]) << '\n';
-    std::cout << "Size of image : " << sizeof(image) << '\n';
+    std::cout << "Size of image : " << dib.RAW_SIZE*3/*sizeof(image)*/ << '\n';
+
+
 
     file.open("../output.bmp", std::ios::out);
-    file.write(dib, 54);
-    file.write(image, sizeof(image));
+    file.write((char*)&dib, 54);
+    file.write(image, dib.RAW_SIZE*3); //sizeof(image));
     file.close();
 
-    //file.open("../hed.mine", std::ios::out);
-    //file.write(dib, 54);//store backup DIB
-    //file.close();
-    //std::cout << (int)(image[269999] & 0xFF);//check if whole array was filled
     return 0;
 }
